3bd2cd866de40b7c88073f53148bc3ab9c9762e6
mmurshidf <murshidm@bu.edu>
2024-09-23 18:35:01 -0400

    updates

--- quotes/urls.py
+++ quotes/urls.py
@@ -7,8 +7,8 @@ from . import views
 
 # all of the URLs that are part of this app
 urlpatterns = [
-    path(r'/', views.quote, name="quote"),
-    path(r'/quote', views.quote, name="quote"),
-    path(r'/show_all', views.show_all, name="show_all"),
-    path(r'/about', views.about, name="about"),
+    path(r'', views.quote, name="quote"),
+    path(r'quote/', views.quote, name="quote"),
+    path(r'show_all/', views.show_all, name="show_all"),
+    path(r'about/', views.about, name="about"),
 ]
