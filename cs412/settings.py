9245bd19c81b3429bd0d42ccbd574fa9c741f8eb
mmurshidf <murshidm@bu.edu>
2024-10-28 13:23:05 -0400

    Update settings.py

--- cs412/settings.py
+++ cs412/settings.py
@@ -143,5 +143,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-APPEND_SLASH = True
