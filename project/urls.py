81b0aebdf78c191015c3163028adda61240c704b
mmurshidf <murshidm@bu.edu>
2024-12-03 07:34:15 -0500

    Homepage

--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from . import views
+from .views import HomePageView, JobDetailView
 
 urlpatterns = [
+    path('', HomePageView.as_view(), name='homepage'),
+    path('job/<int:pk>/', JobDetailView.as_view(), name='job_detail'),
 ]
+
+
--- project/views.py
+++ project/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView
+from .models import JobApplication
 
 # Create your views here.
+
+class HomePageView(ListView):
+    """View to display a list of job applications."""
+    model = JobApplication
+    template_name = 'project/homepage.html'
+    context_object_name = 'job_applications'
+
+    def get_queryset(self):
+        """Return all job applications."""
+        return JobApplication.objects.select_related('company')  # Optimize by selecting related companies
+
+
+class JobDetailView(DetailView):
+    """View to display details of a specific job application."""
+    model = JobApplication
+    template_name = 'project/job_detail.html'
+    context_object_name = 'job'
+
+    def get_queryset(self):
+        """Include company details for the job."""
+        return JobApplication.objects.select_related('company')
+
+
